c2ba51b6a503bd9756367d34342e49e4fe2bad52
Andrea Orlando <andrea4.orlando@mail.polimi.it>
2021-04-04 23:49:17 +0200

    updated Storage and DepotLevel

--- src/main/java/model/DepotLevel.java
+++ src/main/java/model/DepotLevel.java
@@ -19,35 +19,56 @@ public class DepotLevel {
         return maxQuantity;
     }
 
-    public void setResourceType(TypeofResource ToR) {
-        /*
-        aggiungere eccezione nel caso in cui ToR sia diverso da resourceType e quantity sia maggiore di 0
-         */
-        this.resourceType = ToR;
-    }
+    public void setResourceType(TypeofResource ToR) { this.resourceType = ToR; }
 
     public void setMaxQuantity(int q) {
         this.maxQuantity = q;
     }
 
-    public void increaseQuantity(TypeofResource ToR, int q) {
+    public boolean increaseQuantity(TypeofResource ToR, int q) {
 
         /*
-        aggiungere eccezione nel caso in cui (quantity + q) superi maxQuantity
-        aggiungere eccezione nel caso in cui ToR sia diverso da TypeofResource (a meno che quantity = 0)
-        se ToR != TypeofResource e quantity = 0 invocare setResourceType
+        Controlla che ToR corrisponda al tipo di risorsa del deposito e la quantità da aggiungere non ecceda le dimensioni massime consentite
+        Se queste condizioni sono rispettate, la quantità viene incrementata di q
+        Se ToR non corrisponde al tipo di risorsa del deposito ma la quantità vale 0, il tipo di risorsa del deposito diventa ToR e si controlla se la quantità da aggiungere non eccede le dimensioni massime consentite
+        Se si prova ad aggiungere una risorsa di un tipo non consentito o se si eccede nella quantità viene mostrato il messaggio d'errore corrispondente e si restituisce false
          */
 
-        this.quantity = this.quantity + q;
+        if(ToR.equals(this.resourceType)) {
+            if((this.quantity+q) <= this.maxQuantity) { this.quantity = this.quantity + q;
+                return true; }
+            else { System.out.println("Sorry, you can only store" + (this.maxQuantity - this.quantity) + "more" + ToR + "in this depot!");
+                return false; }
+        }
+        else { if (this.quantity==0) {
+                this.setResourceType(ToR);
+                if ((this.quantity + q) <= this.maxQuantity) {
+                    this.quantity = this.quantity + q;
+                    return true; }
+                else { System.out.println("Sorry, you can only store" + (this.maxQuantity - this.quantity) + "more" + ToR + "in this depot!");
+                    return false; }
+            }
+            else { System.out.println("Sorry, you cannot store" + ToR + "in this depot!");
+            return false; }
+        }
     }
 
-    public void decreaseQuantity(TypeofResource ToR, int q) {
+    public boolean decreaseQuantity(TypeofResource ToR, int q) {
 
         /*
-        aggiungere eccezione nel caso in cui (quantity - q) sia inferiore di 0
-        aggiungere eccezione nel caso in cui ToR sia diverso da TypeofResource
+        Controlla che ToR corrisponda al tipo di risorsa del deposito e la quantità sia maggiore di quanto si vuole decrementare il numero di risorse
+
+        Se queste condizioni sono rispettate, la quantità viene decrementata di q e viene restituito true
+        altrimenti viene mostrato il messaggio di errore corrispondente e viene restituito false
          */
 
-        this.quantity = this.quantity - q;
-    }
+        if(ToR.equals(this.resourceType)) {
+            if ((this.quantity - q) > 0) { this.quantity = this.quantity - q;
+            return true; }
+            else { System.out.println("Sorry, you don't have enough" + ToR + "!");
+            return false; }
+        }
+        else { System.out.println("Sorry, this depot does not contain" + ToR + "!");
+            return false; }
+
 }
--- src/main/java/model/Storage.java
+++ src/main/java/model/Storage.java
@@ -6,15 +6,18 @@ public class Storage {
     private int temp = 0;
 
     public void swapLevels(int index1, int index2) {
+
         /*
-        aggiungere eccezione nel caso in cui la quantità di un livello ecceda la quantità massima dell'altro
-        nel caso in cui non venga lanciata l'eccezione:
-        per scambiare il contenuto di due DepotLevel è sufficiente invertire i rispettivi valori di maxQuantity
+        Nel caso in cui la quantità di un livello ecceda la quantità massima dell'altro viene mostrato un messaggio di errore
+        Altrimenti per scambiare il contenuto di due DepotLevel è sufficiente invertire i rispettivi valori di maxQuantity
          */
 
-        temp = levels[index1].getMaxQuantity();
-        levels[index1].setMaxQuantity(levels[index2].getMaxQuantity());
-        levels[index2].setMaxQuantity(temp);
+        if ( levels[index1].getQuantity() > levels[index2].getMaxQuantity() || levels[index2].getQuantity() > levels[index1].getMaxQuantity() ) {
+            System.out.println("Sorry, you cannot swap these depots!"); }
+        else {
+            temp = levels[index1].getMaxQuantity();
+            levels[index1].setMaxQuantity(levels[index2].getMaxQuantity());
+            levels[index2].setMaxQuantity(temp); }
     }
 
     public void add() {
