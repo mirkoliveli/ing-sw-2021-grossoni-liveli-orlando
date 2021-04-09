7e5b9a42c0aaf03c6808c74c30be1aa5fd22fcbc
riccardogrossoni <79752194+riccardogrossoni@users.noreply.github.com>
2021-04-09 17:02:42 +0200

    completato test per FaithTrack, Strongbox e PopesFavorCard, con commenti esplicativi su cosa manca e su come sono strutturati i casi test

--- src/test/java/it/polimi/ingsw/model/FaithTrackTest.java
+++ src/test/java/it/polimi/ingsw/model/FaithTrackTest.java
@@ -6,6 +6,10 @@ import static org.junit.Assert.*;
 
 public class FaithTrackTest {
 
+    //questi primi quattro metodi vengono controllati indirettamente nei due sotto
+    //se verranno modificati e causano una situazione non prevedibile nei casi di movement e activatePopeZone
+    //andranno modificati di conseguenza.
+
     @Test
     public void increasePosition() {
     }
@@ -21,6 +25,17 @@ public class FaithTrackTest {
     @Test
     public void updateScore() {
     }
+
+    //in movement vengono chiamati vari metodi, ad esempio updateScore e IncreasePostion
+    //per come è stato pensato al momento (8/04) movement deve essere l'unico metodo che chiama questi metodi
+    //movement deve effettuare interamente l'azione di spostare il marcatore, aggiornare il punteggio vittoria (se necessario)
+    // e attivare il rapporto in vaticano (ancora da scrivere)
+    //in questo test per ora abbiamo due casi
+    //il primo esegue due movimenti, partendo da zero, e controlla se la posizione e il punteggio alla fine di ognuno dei
+    //movimenti sia corretto
+    //il secondo test verifica che non si vada OutOfBounds quando raggiungiamo la fine del percorso, e controlla se viene
+    //updatato correttamente il punteggio
+    //come riportato sotto andrà costruito un ulteriore caso test per attivare il rapporto in vaticano correttamente
 
     @Test
     public void movement() {
@@ -49,37 +64,70 @@ public class FaithTrackTest {
 
     }
 
+        //testa activatePopeSpace
+        //activateSpacePope è incaricato di, ricevuta la zona da testare, di scartare o girare la carta favore papale
+        //activatePopeSpace utilizza al suo interno checkZone e i metodi flip e discard di PopesFavorCard
+        //venogono verificati 6 casi in totale in cui viene controllato lo stato dei due boolean di PopesFavor
+        //ci sono casi in cui la carta andrà scartata, quindi discarded sarà TRUE, obtained sarà FALSE
+        //ci sono casi in cui la carta andrà flippata, quindi discarded sarà FALSE, obtained sarà TRUE
+
     @Test
     public void flip_discardPopeCardTest(){
 
         //vericano che le tre zone vengono attivate e venga eseguita la giusta operazione
 
-
+        //primo caso, il marker è all'interno della prima zona, e viene invocato il rapporto in vaticano in questa zona
+        //la prima carta "first" verrà quindi girata
         FaithTrack testPopeZone = new FaithTrack(8);
         testPopeZone.activatePopeSpace(1);
         assertTrue(testPopeZone.getFirst().isObtained());
         assertFalse(testPopeZone.getFirst().isDiscarded());
 
+        //secondo caso, il marker è all'interno della seconda zona, e viene invocato il rapporto in vaticano in questa zona
+        //la seconda carta "second" verrà quindi girata
         testPopeZone=new FaithTrack(13);
         testPopeZone.activatePopeSpace(2);
         assertTrue(testPopeZone.getSecond().isObtained());
-        assertFalse(testPopeZone.getFirst().isDiscarded());
+        assertFalse(testPopeZone.getSecond().isDiscarded());
 
+        //terzo caso, il marker è all'interno della terza zona, e viene invocato il rapporto in vaticano in questa zona
+        //la terza carta "third" verrà quindi girata
         testPopeZone=new FaithTrack(22);
         testPopeZone.activatePopeSpace(3);
         assertTrue(testPopeZone.getThird().isObtained());
+        assertFalse(testPopeZone.getThird().isDiscarded());
 
+        //quarto caso, il marker si trova prima della prima zona quando viene invocato il rapporto in vaticano
+        //la prima carta "first" andrà quindi scartata
         testPopeZone = new FaithTrack(3);
         testPopeZone.activatePopeSpace(1);
         assertTrue(testPopeZone.getFirst().isDiscarded());
+        assertFalse(testPopeZone.getFirst().isObtained());
 
+        //quinto caso, il marker si trova prima della seconda zona quando viene invocato il rapporto in vaticano
+        //la seconda carta "second" andrà quindi scartata
         testPopeZone=new FaithTrack(11);
         testPopeZone.activatePopeSpace(2);
         assertTrue(testPopeZone.getSecond().isDiscarded());
+        assertFalse(testPopeZone.getSecond().isObtained());
 
+        //sesto caso, il marker si trova prima della terza zona quando viene invocato il rapporto in vaticano
+        //la terza carta "third" andrà quindi scartata
         testPopeZone=new FaithTrack(14);
         testPopeZone.activatePopeSpace(3);
         assertTrue(testPopeZone.getThird().isDiscarded());
+        assertFalse(testPopeZone.getThird().isObtained());
+
+        //settimo caso, tecnicamente impossibile che si verifichi
+        //vogliamo testare se è possibile girare una carta se il marker si trova dopo la zona
+        //questo è impossibile perchè l'intento è di attivare il rapporto in vaticano nel momento in cui viene raggiunta la casella
+        //questo perchè in movement l'update dello stato viene fatto iterativamente, procedendo di una casella alla volta
+        //quindi la chiamata in vaticano verrà eseguita solo quando il faithmarker si trova prima o al massimo sopra
+        //testiamo comunque per completezza, e per verificare se una volta cambiato il codice rimanga comunque vero il test
+        testPopeZone=new FaithTrack(13);
+        testPopeZone.activatePopeSpace(1);
+        assertTrue(testPopeZone.getFirst().isObtained());
+        assertFalse(testPopeZone.getFirst().isDiscarded());
 
         System.out.println("fine test flip//discard");
     }
--- src/test/java/it/polimi/ingsw/model/StrongboxTest.java
+++ src/test/java/it/polimi/ingsw/model/StrongboxTest.java
@@ -6,6 +6,9 @@ import static org.junit.Assert.*;
 
 public class StrongboxTest {
 
+
+    //test per il costruttore
+    //usato principalmente per provare funzionalità dei tests
     @Test
     public void costruttoretest(){
         Strongbox inizializzazione= new Strongbox();
@@ -16,6 +19,8 @@ public class StrongboxTest {
         System.out.println("fine test costruttore");
     }
 
+    //metodi getters non necessari da testare, getSpecific viene comunque usato ampliamente negli altri tests
+
     @Test
     public void getContents() {
     }
@@ -24,6 +29,10 @@ public class StrongboxTest {
     public void getSpecific() {
     }
 
+    //verifica se funziona il metodo canBuy
+    //questo metodo restituisce true se è possibile pagare un costo (int[4]) passato come parametro
+    //viene testato un caso in cui sia possibile comprare
+    //e un caso in cui non è possibile comprare
     @Test
     public void canBuy() {
         Strongbox testing= new Strongbox(3, 3, 3, 3);
@@ -37,15 +46,22 @@ public class StrongboxTest {
         costo[1]=2;
         costo[2]=2;
         costo[3]=2;
+        //possibile comprare con questo costo
         assertTrue(testing.canBuy(costo));
+
         costo[0]=4;
         costo[1]=2;
         costo[2]=2;
         costo[3]=2;
+        //non possibile comprare con questo costo (costo[0]>contents[0])
         assertFalse(testing.canBuy(costo));
         System.out.println("fine test canBuy");
     }
 
+    //testa se funziona la rimozione di un costo
+    //la rimozione viene eseguita solo se è possibile pagare il costo
+    //vengono portati due casi test, uno in cui è possibile pagare
+    //uno in cui non è possibile pagare
     @Test
     public void remove() {
         Strongbox testing= new Strongbox(3, 3, 3, 3);
@@ -59,6 +75,7 @@ public class StrongboxTest {
         costo[1]=2;
         costo[2]=2;
         costo[3]=2;
+        //qua è possibile pagare, deve rimanere una risorsa per tipo
         assertTrue(testing.remove(costo));
         assertEquals(1, testing.getSpecific(1));
         assertEquals(1, testing.getSpecific(2));
@@ -73,6 +90,10 @@ public class StrongboxTest {
         testing.print();
         System.out.println("end of remove test");
     }
+
+
+    //testa l'aggiunta di risorse nella strongbox dato un parametro costo (int[4]) da aggiungere
+    //viene testato una sola volta in quanto è un metodo semplice che fa solo addizioni, senza condizioni particolari
 
     @Test
     public void store() {
@@ -94,6 +115,9 @@ public class StrongboxTest {
         testing.print();
         System.out.println("end of test store");
     }
+
+    //questi due metodi stampano a video e non servono agli scopi del programma, inoltre vengono testati indirettamente
+    //dentro i test di store e remove.
 
     @Test
     public void print() {
