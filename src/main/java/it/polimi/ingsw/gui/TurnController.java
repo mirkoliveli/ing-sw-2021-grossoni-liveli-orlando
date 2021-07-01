453dfcdb8bdefdc35376aa833aa05eb6aeaca499
Andrea Orlando <andrea4.orlando@mail.polimi.it>
2021-07-01 23:00:13 +0200

    added waiting text

--- src/main/java/it/polimi/ingsw/gui/TurnController.java
+++ src/main/java/it/polimi/ingsw/gui/TurnController.java
@@ -55,6 +55,8 @@ public class TurnController {
     Image servants = new Image("/img/punchboard/servant2.png");
     Image shields = new Image("/img/punchboard/shield2.png");
     Image stones = new Image("/img/punchboard/stone2.png");
+    @FXML
+    private Label waitingmessage;
 
     public void switchToProduction(ActionEvent event) throws Exception {
         /*FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/production.fxml"));
@@ -311,6 +313,8 @@ public class TurnController {
         c11.setFill(colors[10]);
         c12.setFill(colors[11]);
     }
+
+    public void setWaitText() { waitingmessage.setText("It's not your turn!"); }
 
     public void actionDone() {
         cmbutton.setDisable(true);
